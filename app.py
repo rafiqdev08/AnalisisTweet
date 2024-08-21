6e42b04506148c87ae8778d883edf21c0d647c30
rafiqdev08 <rafiq.dev@gmail.com>
2024-08-21 19:52:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,21 +33,21 @@ def main():
     text_clean = preprocess_text(userText)
 
     # Transform text with the model and predict sentiment
-                text_vector = modelsvc_loaded['vectorizer'].transform([text_clean])
-                prediction_proba = modelsvc_loaded['classifier'].predict_proba(text_vector)
+    text_vector = modelsvc_loaded['vectorizer'].transform([text_clean])
+    prediction_proba = modelsvc_loaded['classifier'].predict_proba(text_vector)
                 
-                # Determine sentiment label
-                sentiment_label = 'positif' if proba_positif >= 0.5 else 'negatif'
+    # Determine sentiment label
+    sentiment_label = 'positif' if proba_positif >= 0.5 else 'negatif'
 
-                # Load corresponding image
-                if sentiment_label == 'positif':
-                    image = Image.open('./images/positive.png')
-                else:
-                    image = Image.open('./images/negative.png')
-                image = image.resize((int(image.width / 2), int(image.height / 2)))
+    # Load corresponding image
+    if sentiment_label == 'positif':
+    image = Image.open('./images/positive.png')
+    else:
+    image = Image.open('./images/negative.png')
+    image = image.resize((int(image.width / 2), int(image.height / 2)))
 
-        else:
-            st.warning('Masukkan teks untuk menganalisis.')
+    else:
+    st.warning('Masukkan teks untuk menganalisis.')
 
 if __name__ == '__main__':
     main()
